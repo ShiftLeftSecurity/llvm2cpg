acb35c1063849e0c4f24e6c344785966ee53fbff
AlexDenisov <alex@lowlevelbits.org>
2019-09-18 11:28:20 +0200

    Drop incorrect CPG construction (#12)

--- include/llvm2cpg/CPG/CPGMethod.h
+++ include/llvm2cpg/CPG/CPGMethod.h
@@ -13,26 +13,6 @@ class Type;
 
 namespace llvm2cpg {
 
-/// For debugging/tests only
-std::string valueToString(llvm::Value *value);
-
-class ASTNode {
-public:
-  explicit ASTNode(llvm::Value *value);
-
-  void addChild(ASTNode *child);
-  void setParent(ASTNode *newParent);
-
-  const std::vector<ASTNode *> &getChildren() const;
-  ASTNode *getParent() const;
-  llvm::Value *getValue() const;
-
-private:
-  llvm::Value *value;
-  std::vector<ASTNode *> children;
-  ASTNode *parent;
-};
-
 class CPGMethod {
 public:
   explicit CPGMethod(llvm::Function &function);
@@ -43,8 +23,6 @@ public:
   CPGMethod(const CPGMethod &) = delete;
   CPGMethod &operator=(const CPGMethod &) = delete;
 
-  ASTNode *getRoot() const;
-  ASTNode *getTree(llvm::Value *value) const;
   const std::set<llvm::Type *> &getTypes() const;
   llvm::Type *getReturnType() const;
   const std::string &getName() const;
@@ -52,17 +30,8 @@ public:
   bool isExternal() const;
   const llvm::Function &getFunction() const;
 
-  void dump() const;
-
 private:
-  void constructAST();
-  ASTNode *buildTree(llvm::Value *value);
-  ASTNode *newNode(llvm::Value *value);
-
   llvm::Function &function;
-  std::vector<std::unique_ptr<ASTNode>> ownedNodes;
-  std::map<llvm::Value *, ASTNode *> memoization;
-  ASTNode *root;
   std::set<llvm::Type *> types;
   std::string name;
 };
--- tests/unit-tests/CPGTests.cpp
+++ tests/unit-tests/CPGTests.cpp
@@ -14,17 +14,17 @@ TEST(CPG, empty) {
 TEST(CPG, addBitcode) {
   llvm::LLVMContext context;
   BitcodeLoader loader;
-  auto bitcode = loader.loadBitcode(fixtures::hello_world_c_bc_output_path(), context);
+  auto bitcode = loader.loadBitcode(fixtures::return_constant_c_bc_output_path(), context);
   ASSERT_NE(bitcode.get(), nullptr);
 
   CPG cpg;
   cpg.addBitcode(bitcode.get());
   ASSERT_EQ(cpg.getFiles().size(), size_t(1));
   auto &file = cpg.getFiles().front();
-  ASSERT_EQ(file.getName(), fixtures::hello_world_c_bc_input_path());
+  ASSERT_EQ(file.getName(), fixtures::return_constant_c_bc_input_path());
   ASSERT_EQ(file.getMethods().size(), size_t(1));
   ASSERT_EQ(file.getTypes().size(), size_t(2));
   auto &method = file.getMethods().front();
-  ASSERT_STREQ(method.getName().c_str(), "hello_world");
+  ASSERT_STREQ(method.getName().c_str(), "basic_c_support");
   ASSERT_FALSE(method.isExternal());
 }
